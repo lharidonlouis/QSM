bdb22eff54653cd5023d13bb839bde3d25cd794e
bastck1 <bastien.ck@gmail.com>
2018-02-02 15:49:42 +0100

    je montre un push à Jérome

---
+++ eclipse/src/simulation/Simulation.java
@@ -0,0 +1,17 @@
+package simulation;
+
+import java.util.List;
+
+import line_management.Line;
+import line_management.Train;
+
+public class Simulation implements Runnable {
+	private Line line;
+	private List<Train> trains;
+	
+	@Override
+	public void run() {
+		
+	}
+
+}
---
+++ eclipse/src/simulation/SimulationGUI.java
@@ -0,0 +1,11 @@
+package simulation;
+
+import javax.swing.JFrame;
+
+public class SimulationGUI extends JFrame {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
